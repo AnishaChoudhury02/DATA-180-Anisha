fa64233e797ea45337e25f42349b9d2d2bbf6591
AnishaChoudhury02 <143441207+AnishaChoudhury02@users.noreply.github.com>
2023-09-25 21:46:14 -0400

    package installed

--- dplyrpackage.R
+++ dplyrpackage.R
@@ -1,2 +1,16 @@
 install.packages("dplyr")
-library(dplyr)
+library(MASS)
+install.packages("dplyr")
+#order of package matters, first run mass
+#data wrangling
+ls(Cars93)
+
+Cars93_Ex1 <-select(Cars93, Type, EngineSize,DriveTrain, MPG.city)
+head(Cars93_Ex1)
+table(Cars93$Type)
+
+Cars93_Ex2 <- filter(Cars93, Type=="Small")
+head(Cars93_Ex2)
+
+Cars93_Ex3 <-select(filter(Cars93, Type=="Small"),Model, EngineSize, Horsepower, MPG.highway)
+head(Cars93_Ex3)
